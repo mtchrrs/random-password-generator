2a3a5f2c84435e2dad3b31b88c670bc96933366d
Money Maker <mitchell.harris@westnet.com.au>
2022-08-27 13:16:00 +0800

    commented working process

--- CODE/script.js
+++ CODE/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+  //ask user how long is the password
+  
+  //ask if want to include
+  // uppercase
+  // lowercase 
+  // number
+  // special symbols
+  
+  //generate the password based on the criterias selected and length
+  
+   // 1. create character set based on criterias
+   // 2. loop for the 'length' times, for each iteration,
+   // grab a random char and append to an accumlator
+   
+ 
+  // bank of characters set based off of criteria
+  // eg. random character from sample
+  // lenght --> loop
+  // pw == set of random characters based off criteria
+  
+  //return password
+ 
+ }
+ 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
